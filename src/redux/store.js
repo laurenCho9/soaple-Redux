402003a79b7453120deb782125baed60e172d42e
laurenCho9 <lauren.choux@gmail.com>
2024-03-27 17:36:15 +0900

    feat: redux-thunk 연동해서 Thunk 사용해보기

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,10 @@
 import { applyMiddleware, compose, createStore } from "redux";
 import rootReducer from "./reducers";
-import asyncFunctionMiddleware from "./middlewares/asyncFunctionMiddleware";
+import { thunk as thunkMiddleware } from "redux-thunk";
+// import asyncFunctionMiddleware from "./middlewares/asyncFunctionMiddleware";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(asyncFunctionMiddleware))
-);
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 export default store;
